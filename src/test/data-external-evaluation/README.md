d5476fb87142feb7f45c63ab9fa67f084ca8902d
Eric Pugh <epugh@opensourceconnections.com>
2025-09-06 19:29:55 -0400

    Script demonstrating pairwise import.

--- src/test/data-external-evaluation/README.md
+++ src/test/data-external-evaluation/README.md
@@ -1,4 +1,4 @@
-This directory contains an example of an externally run evaluation being imported into SRW.  The evaluation was run using the Quepid tool (https://github.com/o19s/quepid).
+This directory contains an example of an externally run evaluation being imported into SRW.  The original evaluation was run using the Quepid tool (https://github.com/o19s/quepid) and then exported.
 
 1) Queries, Judgements, and Metrics sourced from a export from Quepid called  `Movie_Search_Quepid_1.json`. 
 
@@ -6,6 +6,6 @@ This directory contains an example of an externally run evaluation being importe
 
 1) The `movies_queryset.json` was done using the Python script `extract_quepid_judgements.py`.
 
-1) The `movies_experiment.json` was done using the Python script `extract_quepid_experiment.py`.
+1) The `movies_experiment_pointwise.json` was done using the Python script `extract_quepid_experiment.py` processing `Movie_Search_Quepid_1.json`.
 
-When the `./scripts/demo_import_pointwise_experiment.sh` is run it creates a version of `movies_experiment.json` called `movies_experiment_updated.json` that has the specific queryset, judgments and search configuration sourced from the running SRW.
+1) The `movies_experiment_pairwise.json` was done using the Python script `generate_pairwise_experiment.py` processing `Movie_Search_Quepid_1.json` and `Movie_Search_Quepid_2.json`..
--- src/test/scripts/demo_import_pairwise_experiment.sh
+++ src/test/scripts/demo_import_pairwise_experiment.sh
@@ -34,16 +34,19 @@ exe curl -s -X PUT "http://localhost:9200/_plugins/_search_relevance/search_conf
 EXTERNAL_SEARCH_CONFIGURATION_ID_2=`jq -r '.search_configuration_id' < RES`
 
 
-# Update the movies_experiment.json file with the extracted IDs
+# Update the movies_experiment_pairwise.json file with the extracted IDs
 echo "Updating experiment file with the extracted IDs..."
-TEMP_FILE="../data-external-evaluation/movies_experiment_updated.json"
+TEMP_FILE="../data-external-evaluation/movies_experiment_tmp.json"
 
 # Use jq to update the JSON file with our variables
 jq --arg qid "$QUERY_SET_ID" \
    --arg sid1 "$EXTERNAL_SEARCH_CONFIGURATION_ID_1" \
    --arg sid2 "$EXTERNAL_SEARCH_CONFIGURATION_ID_2" \
-   '.querySetId = $qid | .searchConfigurationList = [$sid1, $sid2]' \
-   ../data-external-evaluation/movies_experiment_pairwise_sample.json > "$TEMP_FILE"
+   '.querySetId = $qid | 
+    .searchConfigurationList = [$sid1, $sid2] | 
+    (.results[].snapshots[] | select(.searchConfigurationId == "test-search-config-1").searchConfigurationId) = $sid1 |
+    (.results[].snapshots[] | select(.searchConfigurationId == "test-search-config-2").searchConfigurationId) = $sid2' \
+   ../data-external-evaluation/movies_experiment_pairwise.json > "$TEMP_FILE"
 
 if [ $? -eq 0 ]; then
     echo "Successfully updated experiment file with new IDs."    
@@ -55,4 +58,4 @@ fi
 echo "Submitting updated experiment to API..."
 exe curl -s -X POST "http://localhost:9200/_plugins/_search_relevance/experiments" \
 -H "Content-type: application/json" \
---data-binary @../data-external-evaluation/movies_experiment_updated.json
+--data-binary @../data-external-evaluation/movies_experiment_tmp.json
