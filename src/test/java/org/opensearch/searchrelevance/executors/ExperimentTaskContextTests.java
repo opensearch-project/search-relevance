58a429519a244eb5c30a38f7cb33643ab08e4dad
Weijia Zhao <zweijia@amazon.com>
2025-11-21 15:48:46 -0800

    Update ExperimentTaskContextTests to use ThreadLeakScope
    Signed-off-by: Weijia Zhao <zweijia@amazon.com>

--- src/test/java/org/opensearch/searchrelevance/executors/ExperimentTaskContextTests.java
+++ src/test/java/org/opensearch/searchrelevance/executors/ExperimentTaskContextTests.java
@@ -28,6 +28,9 @@ import org.opensearch.searchrelevance.model.ExperimentType;
 import org.opensearch.searchrelevance.model.ExperimentVariant;
 import org.opensearch.test.OpenSearchTestCase;
 
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
+
+@ThreadLeakScope(ThreadLeakScope.Scope.NONE)
 public class ExperimentTaskContextTests extends OpenSearchTestCase {
 
     private ExperimentVariantDao experimentVariantDao;
