3354365eecff33bfa82169f7e6434b2fa26b0516
Eric Pugh <epugh@opensourceconnections.com>
2026-01-23 18:21:04 -0500

    Use constant value instead of magic number
    Signed-off-by: Eric Pugh <epugh@opensourceconnections.com>

--- src/main/java/org/opensearch/searchrelevance/rest/RestSearchQuerySetAction.java
+++ src/main/java/org/opensearch/searchrelevance/rest/RestSearchQuerySetAction.java
@@ -28,6 +28,7 @@ import org.opensearch.rest.BaseRestHandler;
 import org.opensearch.rest.BytesRestResponse;
 import org.opensearch.rest.RestRequest;
 import org.opensearch.search.builder.SearchSourceBuilder;
+import org.opensearch.searchrelevance.common.PluginConstants;
 import org.opensearch.searchrelevance.settings.SearchRelevanceSettingsAccessor;
 import org.opensearch.searchrelevance.transport.queryset.SearchQuerySetAction;
 import org.opensearch.transport.client.node.NodeClient;
@@ -64,7 +65,7 @@ public class RestSearchQuerySetAction extends BaseRestHandler {
         if (request.hasContentOrSourceParam()) {
             searchSourceBuilder.parseXContent(request.contentOrSourceParamParser());
         } else {
-            searchSourceBuilder.query(QueryBuilders.matchAllQuery()).size(10);
+            searchSourceBuilder.query(QueryBuilders.matchAllQuery()).size(PluginConstants.DEFAULTED_QUERY_SET_SIZE);
         }
 
         SearchRequest searchRequest = new SearchRequest().source(searchSourceBuilder);
