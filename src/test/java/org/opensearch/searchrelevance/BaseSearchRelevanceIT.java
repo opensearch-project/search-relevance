be50243dd364db4c41beff962e4df56176311b7d
Prithvi S <prithvisivasankar@gmail.com>
2026-02-18 14:29:14 +0530

    test handling
    Signed-off-by: Prithvi S <prithvisivasankar@gmail.com>

--- src/test/java/org/opensearch/searchrelevance/BaseSearchRelevanceIT.java
+++ src/test/java/org/opensearch/searchrelevance/BaseSearchRelevanceIT.java
@@ -263,7 +263,8 @@ public class BaseSearchRelevanceIT extends OpenSearchRestTestCase {
     }
 
     /**
-     * wipeAllIndices won't work since it cannot delete security index. Use deleteExternalIndices instead.
+     * wipeAllIndices won't work since it cannot delete security index. Use
+     * deleteExternalIndices instead.
      */
     @Override
     protected boolean preserveIndicesUponCompletion() {
@@ -294,6 +295,7 @@ public class BaseSearchRelevanceIT extends OpenSearchRestTestCase {
                 .map(index -> (String) index.get("index"))
                 .filter(indexName -> indexName != null)
                 .filter(indexName -> !indexName.startsWith(INTERNAL_INDICES_PREFIX))
+                .filter(indexName -> !indexName.startsWith("security-auditlog"))
                 .collect(Collectors.toList());
 
             for (final String indexName : externalIndices) {
