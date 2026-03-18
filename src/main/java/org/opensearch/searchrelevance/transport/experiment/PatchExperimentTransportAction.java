c19984a59416c42e8c7f3841619b509f3c08f28e
Prithvi S <prithvisivasankar@gmail.com>
2026-03-18 20:42:41 +0530

    review changes
    Signed-off-by: Prithvi S <prithvisivasankar@gmail.com>

--- src/main/java/org/opensearch/searchrelevance/transport/experiment/PatchExperimentTransportAction.java
+++ src/main/java/org/opensearch/searchrelevance/transport/experiment/PatchExperimentTransportAction.java
@@ -54,6 +54,7 @@ public class PatchExperimentTransportAction extends HandledTransportAction<Patch
                 }
                 Map<String, Object> sourceMap = searchResponse.getHits().getHits()[0].getSourceAsMap();
                 String statusStr = (String) sourceMap.get("status");
+                // Note: This is a TOCTOU (Time-Of-Check to Time-Of-Use) race condition as the status check happens before the partial update.
                 if (AsyncStatus.PROCESSING.name().equals(statusStr)) {
                     listener.onFailure(
                         new SearchRelevanceException(
