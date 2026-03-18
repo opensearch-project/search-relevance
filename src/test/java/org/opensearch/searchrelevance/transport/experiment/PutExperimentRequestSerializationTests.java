aa47b7bffc124d8a59f665d5db07ee1c44f89357
Prithvi S <prithvisivasankar@gmail.com>
2026-03-18 20:52:05 +0530

    review changes!
    Signed-off-by: Prithvi S <prithvisivasankar@gmail.com>

--- src/test/java/org/opensearch/searchrelevance/transport/experiment/PutExperimentRequestSerializationTests.java
+++ src/test/java/org/opensearch/searchrelevance/transport/experiment/PutExperimentRequestSerializationTests.java
@@ -10,7 +10,6 @@ package org.opensearch.searchrelevance.transport.experiment;
 import java.io.IOException;
 import java.util.List;
 
-import org.opensearch.Version;
 import org.opensearch.common.io.stream.BytesStreamOutput;
 import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.searchrelevance.model.ExperimentType;
@@ -18,7 +17,7 @@ import org.opensearch.test.OpenSearchTestCase;
 
 public class PutExperimentRequestSerializationTests extends OpenSearchTestCase {
 
-    public void testSerializationBWC() throws IOException {
+    public void testSerialization() throws IOException {
         PutExperimentRequest request = new PutExperimentRequest(
             ExperimentType.PAIRWISE_COMPARISON,
             "result-id",
@@ -30,32 +29,13 @@ public class PutExperimentRequestSerializationTests extends OpenSearchTestCase {
             10
         );
 
-        // Test with version that supports name and description
-        {
-            BytesStreamOutput out = new BytesStreamOutput();
-            out.setVersion(Version.V_3_6_0);
-            request.writeTo(out);
-            StreamInput in = out.bytes().streamInput();
-            in.setVersion(Version.V_3_6_0);
-            PutExperimentRequest readRequest = new PutExperimentRequest(in);
-            assertEquals("experiment-name", readRequest.getName());
-            assertEquals("experiment-description", readRequest.getDescription());
-            assertEquals(ExperimentType.PAIRWISE_COMPARISON, readRequest.getType());
-            assertEquals("queryset-id", readRequest.getQuerySetId());
-        }
-
-        // Test with version that does NOT support name and description
-        {
-            BytesStreamOutput out = new BytesStreamOutput();
-            out.setVersion(Version.V_3_1_0);
-            request.writeTo(out);
-            StreamInput in = out.bytes().streamInput();
-            in.setVersion(Version.V_3_1_0);
-            PutExperimentRequest readRequest = new PutExperimentRequest(in);
-            assertNull(readRequest.getName());
-            assertNull(readRequest.getDescription());
-            assertEquals(ExperimentType.PAIRWISE_COMPARISON, readRequest.getType());
-            assertEquals("queryset-id", readRequest.getQuerySetId());
-        }
+        BytesStreamOutput out = new BytesStreamOutput();
+        request.writeTo(out);
+        StreamInput in = out.bytes().streamInput();
+        PutExperimentRequest readRequest = new PutExperimentRequest(in);
+        assertEquals("experiment-name", readRequest.getName());
+        assertEquals("experiment-description", readRequest.getDescription());
+        assertEquals(ExperimentType.PAIRWISE_COMPARISON, readRequest.getType());
+        assertEquals("queryset-id", readRequest.getQuerySetId());
     }
 }
